eddc375b274012ee10e82ef5a8366a67523cadf8
yangkt <katie.e.yang@gmail.com>
2019-03-23 17:08:03 -0400

    local changes

--- resnet.py
+++ resnet.py
@@ -13,6 +13,7 @@ import numpy as np
 CAT_CNT = 12
 TRAIN_SIZE = 2732
 VALID_SIZE = 304
+BATCH_SIZE = 32
 
 def create_model():
     
@@ -27,9 +28,9 @@ def create_model():
     n_inputs = res.fc.in_features
     
     #create fully connected layer with 12 out features + activation layer + softmax
-    res.fc = nn.Sequential(nn.Linear(n_inputs, 500),
+    res.fc = nn.Sequential(nn.Linear(n_inputs, 512),
                           nn.ReLU(),
-                          nn.Linear(500, CAT_CNT),
+                          nn.Linear(512, CAT_CNT),
                           nn.ReLU(),
                           nn.LogSoftmax(dim = 1))
     
@@ -44,17 +45,15 @@ def create_dataloaders():
     trainY = pickle.load(open(os.path.join(data_path, "trainY_128" ), "rb"))
     validX = pickle.load(open(os.path.join(data_path, "validX_128" ), "rb"))
     validY = pickle.load(open(os.path.join(data_path, "validY_128" ), "rb"))
-    print(len(trainX))
-    print(len(validX))
+    
     #generate data from the pickled np datasets,transforming to torch tensors
     trainX = np.transpose(trainX, (0,3,2,1))
     validX = np.transpose(validX, (0,3,2,1))
     
     tensor_trainX = Variable(torch.from_numpy(np.array(trainX)).float(), requires_grad=False)
     tensor_trainY = Variable(torch.from_numpy(np.array(trainY)).long(), requires_grad=False)
-    
     train = TensorDataset(tensor_trainX, tensor_trainY)
-    trainLoader = {DataLoader(train, batch_size = 20, shuffle = True)}
+    trainLoader = {DataLoader(train, batch_size = BATCH_SIZE, shuffle = True)}
 
     tensor_validX = torch.stack([torch.Tensor(i) for i in validX])
     tensor_validY = torch.stack([torch.Tensor(i) for i in validY])
@@ -71,7 +70,10 @@ def train_model(model):
     model.train()
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+        model.cuda()
     
     (t_loader, v_loader) = create_dataloaders()
     
@@ -85,10 +87,11 @@ def train_model(model):
         
         running_loss = 0.0
         
+        
         tl = next(iter(t_loader))
         for i, (inputs, labels) in enumerate(tl):
-            inputs= inputs.to(device)
-            labels = labels.to(device)
+            #inputs= inputs.to(device)
+            #labels = labels.to(device)
             
             steps +=1
             
@@ -96,34 +99,40 @@ def train_model(model):
             optimizer.zero_grad()
             
             #forwards + backwards + optimize
-            outputs = model.forward(inputs)
-            _, preds = torch.max(outputs, 1)
-            loss = criterion(outputs, torch.max(labels, 1)[1])
+            outputs = model(inputs)
+            #_, preds = torch.max(outputs, 1)
+            loss = criterion(preds, torch.max(labels, 1)[1])
+            print('train-loss', loss.item(),end=' ')
             loss.backward()
             optimizer.step()
-
-
+            
             # print statistics
-            running_loss += loss.item() * inputs.size(0)
+            running_loss += loss.item()
             running_corrects += torch.sum(preds == torch.max(labels, 1)[1])
+            print(running_corrects/1)
+            
+            
+        epoch_loss = running_loss/(TRAIN_SIZE)
+        epoch_acc = running_corrects.double() / (TRAIN_SIZE)
         
-            print('ok;')
-        epoch_loss = running_loss/TRAIN_SIZE
-        epoch_acc = running_corrects.double() / TRAIN_SIZE
+        vl = next(iter(v_loader))
+        for j, (inputs, labels) in enumerate(vl):
         
         print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc.double()))
             
         print()
-        
-    #running_loss = 0.0
-    #save the model
-    torch.save(model.state_dict(), os.path.join(".", "models"))
     
+    #save the model
+    torch.save(model.state_dict(),".")
+    
+    # While iterating over the dataset do training
+   
+      
+      
 def main():
     model = create_model()
     train_model(model)
     
     
 if __name__ == "__main__":
-    
     main()
